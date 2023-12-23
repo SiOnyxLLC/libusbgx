a0953835a4861ba345fe42b2824872a6c2e14f3e
Charles Gelinas <cgelinas@sionyx.com>
2023-12-23 08:09:47 -0800

    Fix missing format attributes on set

--- src/function/uvc.c
+++ src/function/uvc.c
@@ -1008,6 +1008,8 @@ out:
 
 static int uvc_set_format(char *format_path, const char *format, const struct usbg_f_uvc_format_attrs *attrs)
 {
+	int ret = 0;
+
 	if (attrs->bBitsPerPixel != 0) {
 		ret = usbg_write_dec(format_path, format, "bBitsPerPixel", attrs->bBitsPerPixel);
 		if (ret != USBG_SUCCESS)
