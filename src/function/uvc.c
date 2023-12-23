f6a3cab4e1400f37c98e0d027fc14efefa027d09
Charles Gelinas <cgelinas@sionyx.com>
2023-12-23 09:17:34 -0800

    Fix set ordering

--- src/function/uvc.c
+++ src/function/uvc.c
@@ -1010,7 +1010,7 @@ static int uvc_set_format(char *format_path, const char *format, const struct us
 {
 	int ret = 0;
 
-	if (attrs->guidFormat != NULL) {
+	if (attrs->guidFormat[0] != '\0') {
 		ret = usbg_write_string(format_path, format, "guidFormat", attrs->guidFormat);
 		if (ret != USBG_SUCCESS)
 			return ret;
