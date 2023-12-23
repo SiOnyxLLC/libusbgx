6ade0ecc3d6f9aa589a622c7a808c1a4b9fe90f3
Charles Gelinas <cgelinas@sionyx.com>
2023-12-23 11:25:04 -0800

    Debug code

--- src/function/uvc.c
+++ src/function/uvc.c
@@ -1040,7 +1040,7 @@ static int uvc_set_format(char *streaming_path, const char *format, const struct
 	return ret;
 }
 
-static int uvc_set_frame(char *format_path, const char *format, const struct usbg_f_uvc_frame_attrs *attrs)
+static int uvc_set_frame(char *format_path, const char *format, const struct usbg_f_uvc_frame_attrs *attrs);
 {
 	char frame_path[USBG_MAX_PATH_LENGTH];
 	char full_frame_path[USBG_MAX_PATH_LENGTH];
