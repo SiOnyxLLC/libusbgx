61d1452fbff2e807aa88aa03bd6a63652a6145b6
Charles Gelinas <cgelinas@sionyx.com>
2024-03-06 11:18:27 -0800

    Fix framebased frame

--- src/function/uvc.c
+++ src/function/uvc.c
@@ -1272,9 +1272,9 @@ static int uvc_set_frame_framebased(char *streaming_path, const char *format, co
 	if (ret != USBG_SUCCESS)
 		return ret;
 
-	ret = usbg_write_dec(format_path, frame_name, "dwBytesPerLine", attrs->dwBytesPerLine);
-	if (ret != USBG_SUCCESS)
-		return ret;
+	// ret = usbg_write_dec(format_path, frame_name, "dwBytesPerLine", attrs->dwBytesPerLine);
+	// if (ret != USBG_SUCCESS)
+	// 	return ret;
 
 	ret = usbg_write_dec(format_path, frame_name, "dwDefaultFrameInterval", attrs->dwDefaultFrameInterval);
 	if (ret != USBG_SUCCESS)
